569371b1f86917ed1345c98f30581f70fe524a5f
NarwhalG <rtmoddingteam@gmail.com>
2020-12-08 13:01:40 -0800

    BUGS BUGS BUGS
    probably my most productive and least half-assed commit to date

--- zoomdaddy.py
+++ zoomdaddy.py
@@ -1,20 +1,9 @@
-import subprocess
 import time
-import os
-import logging
-import traceback
+import os, sys, subprocess
+import logging, traceback
 import glob
-import urllib
-import sys
-import urllib.request
+import urllib, urllib.request
 from datetime import date, datetime
-
-def death():
-    os.system('cls')
-    if os.path.isfile("ignore\skull.txt"):
-        f = open('ignore\skull.txt', 'r', encoding='utf-8')
-        print(f.read())
-        f.close()
 
 def default_print():
     print(f"{Fore.GREEN}Modules imported succesfully! {Style.RESET_ALL}ZoomSlob {Fore.LIGHTYELLOW_EX}Version {version}{Style.RESET_ALL}")
@@ -34,23 +23,24 @@ def default_print():
     time.sleep(1)
     if len(csv.index) > 0:
         print("Found " + str(len(csv.index)) + " class(es), time will be checked regularly in the background.")
-version = 1.3
+version = 1.4
 def update_checker():
     try:
         print("Checking for updates...")
+        time.sleep(1.5)
         stream = urllib.request.urlopen("https://raw.githubusercontent.com/NarwhalG/ZoomSlob/main/version.txt")
         streamBytes = stream.readlines()
         streamText = streamBytes[0].decode('utf8')
         streamText = str(streamText).split(' ')
         stream.close()
-        streamVer = streamText[0]
+        streamVer = float(streamText[0])
         streamReq = streamText[1]
-        time.sleep(1.5)
         os.system('cls')
-        if float(streamVer) > version or streamReq == "1":
+        if streamVer > version or streamReq == "1":
             print(f"The newer version of ZoomSlob ({Fore.YELLOW}{streamVer}{Style.RESET_ALL}) is available")
             upChoice = ""
-            if streamReq == "1": # in case i get really sloppy with my commits
+            if streamReq == "1" and streamVer > version: # in case i get really sloppy with my commits
+                print("Downloading required update...")
                 upChoice == "y"
             while upChoice == "":
                 tempChoice = input(f'Would you like to update now? ({Fore.GREEN}y{Style.RESET_ALL}/{Fore.RED}n{Style.RESET_ALL}) ').lower()
@@ -100,6 +90,7 @@ def update_checker():
         print(f"{Fore.YELLOW}Failed to check for updates.{Style.RESET_ALL}")
         logging.error(traceback.format_exc())
         input('\nPress enter to continue anyway') 
+# Make sure all modules are installed
 try:
     import pyautogui
     from PIL import Image
@@ -109,44 +100,8 @@ try:
 
     pyautogui.FAILSAFE = True
     os.system('color')
-    update_checker()
-    csv = None
-
-    csvDir = 'csv/'
-    if os.path.exists(csvDir) and os.path.isdir(csvDir):
-        if not os.listdir(csvDir):
-            print("You do not have any schedules setup. Let's make one!")
-            time.sleep(4)
-            if os.path.isfile("schedule_maker.py"):
-                subprocess.call(['python', 'schedule_maker.py'])
-            if os.path.isfile("zoomdaddy.py"):
-                os.system('cls')
-                subprocess.call(['python', 'zoomdaddy.py'])
-            exit()
-        else:
-            csvList = glob.glob('csv\*.{}'.format('csv'))
-            if len(csvList) == 1:
-                choiceNum = 0
-                csv = pd.read_csv(csvList[0])
-            else:
-                print("Multiple .csv files found, choose one")
-                choices = []
-                trash = "csv\\"
-                for idx in range(len(csvList)):
-                    print(f"[{Fore.CYAN}{str(idx + 1)}{Style.RESET_ALL}] {csvList[idx].replace(trash, '')}")
-                    choices.append(int(idx))
-                while True:
-                    csvChoice = input()
-                    try:
-                        choiceNum = int(csvChoice)
-                        if choiceNum - 1 in choices:
-                            csv = pd.read_csv(csvList[choiceNum - 1])
-                            break
-                    except:
-                        pass
-    os.system('cls')
 except ModuleNotFoundError as err:
-    death()
+    os.system('cls')
     time.sleep(2)
     if os.path.isfile("module_installer.py"):
         subprocess.call(['python', 'module_installer.py'])
@@ -158,14 +113,84 @@ except ModuleNotFoundError as err:
         input()
     exit()
 
-if os.path.isfile('hahasecret'):
-    pyautogui.press('enter')
-    time.sleep(1.5)
-    if os.path.isfile("ignore\pin.txt"):
-        f = open('ignore\pin.txt', 'r', encoding='utf-8')
-        pin = f.read()
+if not os.path.isfile('hahasecret'):
+    update_checker()
+
+dir_path = 'ignore'
+zoom_path = ""
+if os.path.exists(dir_path) and os.path.isdir(dir_path):
+    txt_path = dir_path + '\\zoomDir.txt'
+    if os.path.exists(txt_path) and os.path.isfile(txt_path):
+        f = open(txt_path, 'r', encoding='utf-8')
+        zoom_dir = f.read()
         f.close()
-        pyautogui.write(pin)
+        if os.path.exists(zoom_dir) and os.path.isfile(zoom_dir) and zoom_dir.endswith('Zoom.exe'):
+            zoom_path = zoom_dir
+    else:
+        try:
+            subfolders = [f.path for f in os.scandir("C:\\Users") if f.is_dir()]
+            for i in subfolders:
+                if os.path.isfile(i + "\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe"):
+                    zoom_path = i + "\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe"
+                    dir_file = open('ignore\\zoomDir.txt', 'w')
+                    dir_file.write(zoom_path)
+                    dir_file.close()
+                    break
+        except:
+            pass
+while zoom_path == "":
+    os.system('cls')
+    tempPath = input(f'Please enter your Zoom directory, it should end with something like {Fore.LIGHTBLUE_EX}Zoom\\bin\\Zoom.exe{Style.RESET_ALL}\n')
+    if tempPath != "" and os.path.exists(tempPath):
+        if os.path.isdir(tempPath) and os.path.exists(tempPath + '\\Zoom.exe') and os.path.isfile(tempPath + '\\Zoom.exe'):
+            dir_file = open('ignore\\zoomDir.txt', 'w')
+            dir_file.write(tempPath + '\\Zoom.exe')
+            dir_file.close()
+            zoom_path = tempPath + '\\Zoom.exe'
+        elif os.path.isfile(tempPath) and tempPath.endswith('Zoom.exe'):
+            dir_file = open('ignore\\zoomDir.txt', 'w')
+            dir_file.write(tempPath)
+            dir_file.close()
+            zoom_path = tempPath
+# Chooses the csv to use with automation
+csv = None
+csvDir = 'csv/'
+if os.path.exists(csvDir) and os.path.isdir(csvDir):
+    csvList = glob.glob('csv\*.{}'.format('csv'))
+    if not os.listdir(csvDir) or len(csvList) == 0:
+        if os.path.isfile("schedule_maker.py"):
+            print("You do not have any schedules setup. Let's make one!")
+            time.sleep(3.5)
+            subprocess.call(['python', 'schedule_maker.py'])
+            if os.path.isfile("zoomdaddy.py"):
+                os.system('cls')
+                subprocess.call(['python', 'zoomdaddy.py'])
+            exit()
+        else:
+            print("You are missing 'schedule_maker.py'. Please make sure it's in the same directory or redownload ZoomSlob.\n")
+            input('Press enter to exit')
+            exit()
+    else:
+        if len(csvList) == 1:
+            choiceNum = 0
+            csv = pd.read_csv(csvList[0])
+        else:
+            print("Multiple .csv files found, choose one")
+            choices = []
+            trash = "csv\\"
+            for idx in range(len(csvList)):
+                print(f"[{Fore.CYAN}{str(idx + 1)}{Style.RESET_ALL}] {csvList[idx].replace(trash, '')}")
+                choices.append(int(idx))
+            while True:
+                csvChoice = input()
+                try:
+                    choiceNum = int(csvChoice)
+                    if choiceNum - 1 in choices:
+                        csv = pd.read_csv(csvList[choiceNum - 1])
+                        break
+                except:
+                    pass
+os.system('cls')
 
 def time_difference(theTime):
     curt = time.localtime()
@@ -176,6 +201,7 @@ def time_difference(theTime):
     tdelta = datetime.strptime(curTime, '%H:%M:%S') - datetime.strptime(str(m1), '%H:%M:%S')
     return tdelta.total_seconds()
 
+# gets rid of meetings older than 5 minutes
 curDay = datetime.today().strftime('%A').lower()[:2]
 for index, row in csv.iterrows():
     meetingDays = row['MeetingDays']
@@ -188,23 +214,22 @@ for index, row in csv.iterrows():
             csv.drop(index, inplace = True)
 
 default_print()
-
-startTime = time.time()
+def process_exists(process):
+    call = ['TASKLIST', '/FI', f'imagename eq {process}']
+    output = subprocess.check_output(call).decode()
+    last = output.strip().split('\r\n')[-1]
+    return last.lower().startswith(process.lower())
 def start_zoom():
+    if process_exists('Zoom.exe'):
+        try:
+            os.system("taskkill /f /im Zoom.exe")
+        except Exception:
+            pass
     try:
-        os.system("taskkill /f /im Zoom.exe")
-    except Exception:
-        pass
-        
-    subfolders = [f.path for f in os.scandir("C:\\Users") if f.is_dir()]
-    try:
-        for i in subfolders:
-            if os.path.isfile(i + "\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe"):
-                subprocess.Popen(i + "\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
-                print("Launching Zoom")
-                break
+        subprocess.Popen(zoom_path)
+        print("Launching Zoom")
     except Exception as e:
-        death()
+        os.system('cls')
         print(f"{Fore.RED}There was a problem finding the directory for Zoom.exe!{Style.RESET_ALL}")
         logging.error(traceback.format_exc())
         input('Press enter to close the program.')
@@ -239,12 +264,11 @@ def manual_sign_in(meetid, passwd = ""):
         pyautogui.write(passwd)
         pyautogui.press('enter')
 
+# check for a recently missed meeting and join it
 if not csv.empty:
     for index, row in csv.iterrows():
-        print(index)
-        # -300
+        # if less than 5 minutes late: join
         if time_difference(row['MeetingTime']) <= 300:
-            #less than 5 minutes late, join late meeting
             m_id = str(row['MeetingID'])
             m_id.replace(" ", "") #just makes life less complicated
             m_pswd = str(row['MeetingPassword'])
@@ -260,6 +284,7 @@ if not csv.empty:
                 print(f"{Fore.RED}Something went wrong with the automation!{Style.RESET_ALL}")
                 logging.error(traceback.format_exc())
             break # prevent madness
+# checks the remaining meetings every 10 seconds
 while True:
     if len(csv.index) == 0:
         break
